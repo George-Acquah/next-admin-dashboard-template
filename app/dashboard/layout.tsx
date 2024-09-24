d23ad8efd12cdedb7966e982ec5bcf8057afcbca
George Acquah <92464817+George-Acquah@users.noreply.github.com>
2024-09-24 13:16:15 +0000

    fixed sidebar scrolling bug

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,10 @@
 import { Typography } from "@/components/ui/typography";
-
 import { cn } from "@/utils/classes.utils";
+import { THEME } from "@/utils/constants";
 import dynamic from "next/dynamic";
 
 const Search = dynamic(() => import("@/components/search"), {
-  loading: () => <Typography variant="span">Loading...</Typography>
+  loading: () => <Typography variant="span">Loading...</Typography>,
 });
 
 const Sidebar = dynamic(() => import("@/components/navigation/sidebar"), {
@@ -22,13 +22,18 @@ export default function DashboardLayout({ children }: _IChildren) {
   return (
     <div
       className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  border border-neutral-200 dark:border-neutral-700",
-        "h-[100vh]"
+        "flex md:flex-row bg-gray-100 dark:bg-neutral-800 w-full border border-neutral-200 dark:border-neutral-700",
+        "h-screen" // Changed to h-screen for full height
       )}
     >
-      <Sidebar />
-      <div className="flex flex-1">
-        <div className="p-2 md:px-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+      <div className="flex-none">
+        <Sidebar />
+      </div>
+      <div className="flex-1 overflow-y-auto">
+        {/* Enable vertical scroll */}
+        <div
+          className={`p-2 md:px-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 ${THEME.mainBg} flex flex-col gap-2`}
+        >
           <div className="md:mb-4 flex flex-row justify-between items-center">
             <Search entityType="QUERY" />
             <NavbarDropdowns
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,26 +1,44 @@
+import EventCalendar from "@/components/calendars/eventCalendar";
 import InformationCards from "@/components/cards/infoCards";
+import CountChart from "@/components/charts/countChart";
+import FinanceChart from "@/components/charts/financeChart";
+import TeamBarChart from "@/components/charts/teamsChart";
+import { entityData } from "@/data/root.data";
 
 const Dashboard = ({ searchParams }: _ISearchQuery) => {
-    const user = searchParams?.q || "";
-    const currentPage = Number(searchParams?.page) || 1;
-    const pageSize = Number(searchParams?.size) || 5;
+  const user = searchParams?.q || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const pageSize = Number(searchParams?.size) || 5;
 
-    console.log(user, currentPage, pageSize);
+  console.log(user, currentPage, pageSize);
   return (
-    <div className="p-4 flex gap-4 flex-col md:flex-row">
+    <div className="flex gap-4 flex-col md:flex-row">
       {/* Right */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* Dashboard Cards */}
-        <InformationCards />
+        <div className="flex gap-4 justify-between flex-wrap">
+          {entityData.map((entity, idx) => (
+            <InformationCards key={idx} entity={entity} />
+          ))}
+        </div>
+        {/* Analytics */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <TeamBarChart />
+          </div>
+        </div>
+        {/* Bottom Finance */}
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
       {/* Left */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        {Array.from({ length: 2 }, (_, i) => (
-          <div
-            key={"second-array" + i}
-            className="h-40 md:h-full w-full rounded-lg bg-gray-100 dark:bg-neutral-800 "
-          ></div>
-        ))}
+        <EventCalendar />
       </div>
     </div>
   );
