69c6ff5bcf23b3dfbacec9cdf7ae81f1e397a941
George Acquah <92464817+George-Acquah@users.noreply.github.com>
2024-09-26 15:45:28 +0000

    Create projectsTable.tsx

---
+++ components/tables/projectsTable.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import TableComponent from "./tableComponent"; // Import the reusable TableComponent
+import { projectsData } from "@/data/dummy.data";
+import { mockFetchData } from "@/lib/dataFetching";
+import { deleteEntity } from "@/lib/actions";
+
+const ProjectsTable = async ({query, currentPage, pageSize}: _ISpecificTableProps) => {
+  const projects = await mockFetchData(projectsData, { query, currentPage, pageSize})
+
+  const columns = ["title", "description", "teamMembers", "isActive"]; // Specify columns to display
+  return (
+    <TableComponent
+      data={projects}
+      columnData={columns}
+      entityType="project"
+      deleteAction={deleteEntity}
+      // onBulkUpdate={updateBulkEntity}
+    />
+  );
+};
+
+export default ProjectsTable;
