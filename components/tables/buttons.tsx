5f858dec435991568f42397eac2502a41dcde1fd
George Acquah <92464817+George-Acquah@users.noreply.github.com>
2024-09-26 15:46:00 +0000

    created buttons and badges for tables

---
+++ components/tables/buttons.tsx
@@ -0,0 +1,37 @@
+import { PencilIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { DeleteClientBtn } from "../client/clientButtons";
+import { cn } from "@/utils/classes.utils";
+import { EyeIcon } from "@heroicons/react/24/outline";
+
+export function EditBtn({
+  href,
+  className,
+}: {
+  href: string;
+  className?: string;
+}) {
+  return (
+    <Link href={href} aria-label={href}>
+      <button
+        aria-label={href}
+        className={cn(
+          "w-7 h-7 flex items-center justify-center rounded-full text-primary-foreground bg-blue-200 dark:bg-blue-300",
+          className
+        )}
+      >
+        <EyeIcon className="w-4 h-4 font-black" />
+      </button>
+    </Link>
+  );
+}
+
+export function DeleteBtn({ id, label, action, path }: _IActionBtn) {
+  const deleteAction = action.bind(null, id, path ?? "");
+
+  return (
+    <form action={deleteAction}>
+      <DeleteClientBtn label={label} />
+    </form>
+  );
+}
---
+++ components/tables/status.tsx
@@ -0,0 +1,49 @@
+import { Typography } from "../ui/typography";
+
+interface StatusBadgeProps {
+  status: boolean | string | number | undefined; // Allow flexible status types
+  trueLabel?: string; // Optional label for `true` values (for boolean statuses)
+  falseLabel?: string; // Optional label for `false` values (for boolean statuses)
+  styleMap?: { [key: string]: string }; // Custom styles based on status values
+}
+
+const defaultStyles = {
+  true: "bg-green-700 text-white", // Default style for "true" status
+  false: "bg-red-600 text-white", // Default style for "false" status
+  default: "bg-gray-300 text-black", // Fallback for unknown statuses
+};
+
+// Dynamic StatusBadge that handles different types of status values
+const StatusBadge: React.FC<StatusBadgeProps> = ({
+  status,
+  trueLabel = "Active",
+  falseLabel = "Inactive",
+  styleMap = {},
+}) => {
+  let displayText;
+  let statusStyle;
+
+  if (typeof status === "boolean") {
+    // If status is boolean, map it to true/false labels and styles
+    displayText = status ? trueLabel : falseLabel;
+    statusStyle = status ? defaultStyles.true : defaultStyles.false;
+  } else if (typeof status === "string" || typeof status === "number") {
+    // If status is a string or number, use it directly
+    displayText = String(status);
+    statusStyle = styleMap[displayText] || defaultStyles.default;
+  } else {
+    // Fallback for undefined or null values
+    displayText = "Unknown";
+    statusStyle = defaultStyles.default;
+  }
+
+  return (
+    <Typography variant="span"
+      className={`inline-block px-3 py-1 rounded-full font-mono font-semibold uppercase ${statusStyle}`}
+    >
+      {displayText}
+    </Typography>
+  );
+};
+
+export default StatusBadge;
