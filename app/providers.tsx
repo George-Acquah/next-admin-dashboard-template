d4cbf84fc2f965d6fe5fc400a6be7fd90425a679
George Acquah <92464817+George-Acquah@users.noreply.github.com>
2024-09-28 21:44:55 +0000

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,27 +3,18 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
-import SidebarProvider from "@/utils/contexts/sidebar.context";
 import ModalProvider from "@/utils/contexts/modal.context";
 import ConfiguratorProvider from "@/utils/contexts/configurator.context";
-interface _IProviderProps extends ThemeProviderProps {
-  animate?: boolean;
-}
+type _TProviderProps = ThemeProviderProps;
 
-export default function Providers({
-  children,
-  animate = true,
-  ...props
-}: _IProviderProps) {
+export default function Providers({ children, ...props }: _TProviderProps) {
   return (
     <NextThemesProvider
       {...props}
       disableTransitionOnChange // Ensures no theme flickering on change
     >
       <ConfiguratorProvider>
-        <SidebarProvider animate={animate}>
-          <ModalProvider>{children}</ModalProvider>
-        </SidebarProvider>
+        <ModalProvider>{children}</ModalProvider>
       </ConfiguratorProvider>
     </NextThemesProvider>
   );
